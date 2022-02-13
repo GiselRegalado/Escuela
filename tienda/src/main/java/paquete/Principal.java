98acbefdba4671fa66dffd02d6d0256c4a7833ab
Axel Lopez <axelllopez2002@gmail.com>
2022-02-12 18:54:15 -0600

    tienda

---
+++ tienda/src/main/java/paquete/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete;
+
+/**
+ *
+ * @author axellopez
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Ventana v1 = new Ventana();
+        v1.setVisible(true);
+    }
+}
